1642a8e6ec8f93602035eeeecc1e252e89113be1
Manuel0815 <ma.ho.1@gmx.at>
2018-12-02 19:18:49 +0100

    remove single function files

--- disemvowel.py
+++
@@ -1,9 +0,0 @@
-def disemvowel(string):
-    s = ""
-    chars = ["A", "E", "I", "O", "U"]
-    for c in string:
-        if c.capitalize() not in chars:
-            s += c
-    return s
-
-print disemvowel ("This website is for losers LOL!")
--- is_triangle.py
+++
@@ -1,8 +0,0 @@
-def is_triangle(a, b, c):
-    l = [a, b, c]
-    l.sort()
-    if l[0] + l[1] <= l[2]:
-        return False
-    return True
-
-print is_triangle(1, 1, 3)
