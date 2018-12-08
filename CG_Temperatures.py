1b46c4536d51a652bd9f626be61edaf80fae3006
Manuel0815 <ma.ho.1@gmx.at>
2018-12-09 00:13:17 +0100

    mars lander

--- CG_Temperatures.py
+++ CG_Temperatures.py
@@ -4,12 +4,18 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+index = 0
+closestTemp = None
+
 n = int(raw_input())  # the number of temperatures to analyse
-for i in raw_input().split():
+arr = raw_input().split()
+for i in arr:
+    i = int(i)
     # t: a temperature expressed as an integer ranging from -273 to 5526
-    t = int(i)
+    if (closestTemp == None) or (abs(i) < abs(closestTemp)) or (abs(closestTemp) == abs(i) and i > 0 and closestTemp < 0):
+        closestTemp = i
 
 # Write an action using print
-# To debug: print >> sys.stderr, "Debug messages..."
+# To debug: print >> sys.stderr, "Debug messages...
 
-print "result"
+print closestTemp or 0
